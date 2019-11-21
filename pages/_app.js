fa5614cdd424b099f6a6be109020575a3d3b1a4c
George Bougakov <githubinq@bygeorgenet.me>
2019-11-21 12:47:50 +0300

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,20 @@ import '../styles/index.css'
 
 import React from 'react'
 import App from 'next/app'
+import Router from 'next/router'
+import ReactGA from 'react-ga'
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
 
     return <Component {...pageProps} />
+  }
+
+  componentDidMount() {
+    ReactGA.initialize('UA-153094136-1')
+    Router.events.on('routeChangeComplete', url => {
+      ReactGA.pageview(url)
+    })
   }
 }
